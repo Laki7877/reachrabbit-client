c730ca0e4a255b5b8898acabfb4d338830dbc65b
poonwu <poon.wuthi@gmail.com>
2016-06-19 01:57:09 +0700

    Added express server

---
+++ app.js
@@ -0,0 +1,37 @@
+/**
+ * Express app for serving web
+ *
+ * @author     Poon Wu <poon.wuthi@gmail.com>
+ * @since      0.0.1
+ */
+'use strict';
+
+// set process env from .env
+require('dotenv').config();
+
+var app   = require('express')(),
+    path  = require('path'),
+    http  = require('http'),
+    morgan= require('morgan');
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', path.resolve(__dirname, 'public'));
+app.use(morgan('dev'));
+app.use(express.static(path.resolve(__dirname, 'public/assets')));
+
+/************************************
+ * Route
+ ************************************/
+
+app.get('/', function(req, res) {
+  return res.render('index');
+});
+
+
+/************************************
+ * Start server
+ ************************************/
+http.createServer(app).listen(process.env.PORT || 8080, function() {
+  console.log('Express server listening to port ' + (process.env.PORT || 8080));
+});

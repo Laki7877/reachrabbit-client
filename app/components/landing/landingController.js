65d90a3f30a89e796caded26c3b3ff85ed28b39a
poonwu <poon.wuthi@gmail.com>
2016-06-19 01:57:22 +0700

    Added angular component for poc

---
+++ app/components/landing/landingController.js
@@ -0,0 +1,12 @@
+/**
+ * Landing page controller
+ *
+ * @author     Poon Wu <poon.wuthi@gmail.com>
+ * @since      0.0.1
+ */
+'use strict';
+
+angular.module('app.landing')
+  .controller('LandingController', function($scope) {
+
+  });

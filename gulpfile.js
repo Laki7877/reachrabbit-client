fd072c1de0a65cf6e37118c63183d7a3dd97ee35
Poon Wu <poon.wuthi@gmail.com>
2016-06-21 18:31:53 +0700

    Added prehook lint

--- gulpfile.js
+++ gulpfile.js
@@ -69,7 +69,13 @@ var liveReload = true;
  ***************************************************/
 
 // git precommit
-gulp.task('pre-commit', 'Git hook pre-commit', ['lint']);
+gulp.task('pre-commit', 'Git hook pre-commit', function() {
+  return gulp.src(guppy.src('pre-commit'))
+    .pipe(plugins.filter(['*.js']))
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter(require('jshint-stylish')))
+    .pipe(plugins.jshint.reporter('fail'));
+});
 
 /*******************************************
  * Test

7cc717248ba1a36930543b901ee4d271b0dc6f06
Poon Wu <poon.wuthi@gmail.com>
2016-06-21 18:25:24 +0700

    Added dependencies

--- app/components/common/index.js
+++ app/components/common/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app.common', []);
+angular.module('app.common', ['angularCSS', 'ui.router']);
 
 var bulk = require('bulk-require');
 bulk(__dirname, ['**/*.js', '!index.js']);

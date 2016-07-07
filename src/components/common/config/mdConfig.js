248f8868891a979313186f913387f91caf6f1037
Pat Sabpisal <ssabpisa@me.com>
2016-07-07 19:53:09 +0700

    Better UI

--- src/components/common/config/mdConfig.js
+++ src/components/common/config/mdConfig.js
@@ -9,7 +9,20 @@
 angular.module('app.common')
   .config(function($mdThemingProvider, $mdIconProvider) {
     $mdThemingProvider.theme('default')
+     .primaryPalette('teal', {
+      'default': '600',
+      'hue-1': '100',
+      'hue-2': '900',
+      'hue-3': '500'
+    })
     .accentPalette('deep-orange');
     $mdIconProvider.fontSet("material-icons")
+
+    //Dark theme
+    $mdThemingProvider.theme('duck')
+     .primaryPalette('blue-grey', {
+      'default': '900'
+    })
+
 
   });

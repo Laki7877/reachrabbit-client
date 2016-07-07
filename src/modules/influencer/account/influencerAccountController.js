c4e1614901dafe064e688d4204c0cad89410aeb1
Pat Sabpisal <ssabpisa@me.com>
2016-07-07 15:30:47 +0700

    Profile

--- src/modules/influencer/account/influencerAccountController.js
+++ src/modules/influencer/account/influencerAccountController.js
@@ -52,12 +52,14 @@ angular.module('app.influencer')
   * Influencer Profile
   *
   */
-  .controller('influencerAccountProfileController', function($scope,$uploader, $api) {
+  .controller('influencerAccountProfileController', function($scope,$uploader, $auth, $mdToast, $mdDialog, $api) {
     //Hacky, will change after TODO: Friday
     document.getElementsByTagName("body")[0].style.backgroundImage = "none";
     document.getElementsByTagName("body")[0].style.backgroundColor = "#ebebeb";
 
-    $scope.formData = {};
+    $scope.formData = {
+      socialAccounts: {}, selectedTopics: []
+    };
 
     $scope.upload = function(file) {
       $scope.loadingImage = true;
@@ -68,6 +70,48 @@ angular.module('app.influencer')
         });
     };
 
+    $scope.topicExists = function(item, selected) {
+      return selected.indexOf(item.id) !== -1;
+    }
+
+    $scope.topicDisabled = function(item, selected) {
+      return selected.length >= 3 && !$scope.topicExists(item, selected)
+    }
+    $scope.topicToggle = function(item, selected) {
+      if ($scope.topicDisabled(item, selected)) return;
+      if ($scope.topicExists(item, selected)) {
+        _.remove(selected, function(x) {
+          return x == item.id
+        })
+      } else {
+        selected.push(item.id)
+      }
+    }
+    $scope.topics = [{
+      name: "Travel",
+      id: 1
+    }, {
+      name: "Beauty",
+      id: 2
+    }, {
+      name: "Food",
+      id: 3
+    }, {
+      name: "Cooking",
+      id: 4
+    }, {
+      name: "Gaming",
+      id: 5
+    }, {
+      name: "Gadget",
+      id: 6
+    }, {
+      name: "Educational",
+      id: 9
+    }];
+
+
+    //get user info
     $api({
       method: 'GET',
       url: '/me'
@@ -76,6 +120,32 @@ angular.module('app.influencer')
     }).catch(function(err) {
       console.log(err);
     });
+
+
+    function authNOK(err) {
+      if (err.data && err.data.display) {
+        alert(err.data.display.message)
+      }
+    }
+
+    //Other functions
+    $scope.linkedWith = function(key) {
+      return key in $scope.formData.socialAccounts;
+    };
+
+    $scope.linkWith = function(key) {
+      $auth.authenticate(key)
+        .then(function(res) {
+          $mdToast.show(
+            $mdToast.simple()
+            .textContent('Linked to ' + key + ' account')
+            .position('top right')
+            .hideDelay(3000)
+          );
+        })
+        .catch(authNOK);
+    };
+
 
   })
   /*
@@ -169,7 +239,6 @@ angular.module('app.influencer')
           );
         })
         .catch(authNOK);
-
     };
 
     // go back

8c6416ee28f133a60c781bf5a3974dfdf69e57e1
poonwu <poon.wuthi@gmail.com>
2016-07-21 02:21:28 +0700

    Added admin pages

---
+++ src/adminApp.js
@@ -0,0 +1,16 @@
+/**
+ * Influencer app
+ *
+ * @author     Poon Wu <poon.wuthi@gmail.com
+ * @since      0.0.1
+ */
+'use strict';
+
+var components = [
+  require('./modules/admin')
+];
+
+angular.module('app', components)
+  .run(function($state, $json) {
+
+  });

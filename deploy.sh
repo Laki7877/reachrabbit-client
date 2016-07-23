69fa263ace068b0851ce9ad75c076a188977d8c2
Pat Sabpisal <ssabpisa@me.com>
2016-07-24 00:55:50 +0700

    DEployment script typo

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 #
 git merge -m "Merge master -> develop, choosing develop" -s ours master 
 git checkout master
-git merge -m "Merge develop -> master" master
+git merge -m "Merge develop -> master" develop
 cp .env.heroku .env
 gulp build:all
 git add -A

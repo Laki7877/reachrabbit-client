f2b6a63707d4dbd3ff1ce734c19b66705169e9fb
Pat Sabpisal <ssabpisa@me.com>
2016-07-24 01:06:48 +0700

    gh herok

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ git merge -m "Merge master -> develop, choosing develop" -s ours master
 git checkout master
 git merge -m "Merge develop -> master" develop
 cp .env.heroku .env
+cp .gitignore.heroku .gitignore
 gulp build:all
 git add -A
 git commit -m "Heroku Deployment $1"

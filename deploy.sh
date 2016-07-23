169928ce317c57a9c819affafa1edc0613762ce0
Pat Sabpisal <ssabpisa@me.com>
2016-07-23 22:03:34 +0700

    D script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,10 @@
 #
 git merge -m "" -s ours master 
 git checkout master
-git checkout develop
 cp .env.heroku .env
 gulp build:all
 git add -A
 git commit -m "Heroku Deployment $1"
 git push heroku
-git push master
+git push origin
 git checkout develop

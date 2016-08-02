b96efb5d72208cc95a84914a5064574c360ac411
Pat Sabpisal <ssabpisa@me.com>
2016-08-02 17:28:42 +0700

    wrongbr

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 #
 git merge -m "Merge master -> develop, choosing develop" -s ours master 
 git checkout master
-git merge -m "Merge develop -> master" develop
+git merge -m "Merge develop -> master" ui-dev
 git add -A
 git commit -m "Heroku Deployment $1"
 git push heroku

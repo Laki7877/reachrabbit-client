f7b9eb456549177ddbf1c03463fba7b45f31b722
Pat Sabpisal <ssabpisa@me.com>
2016-09-11 10:56:33 +0700

    Win size

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -5,6 +5,8 @@ var Chance = require('chance');
 var chance = new Chance();
 var EC = protractor.ExpectedConditions;
 
+browser.driver.manage().window().setSize(1200, 800);
+
 describe('Brand', function () {
 
     beforeAll(function () {

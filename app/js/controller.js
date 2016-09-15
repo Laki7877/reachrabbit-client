513a8b93b9cf76041a034479c96d000929b1d7bd
Pat Sabpisal <ssabpisa@me.com>
2016-09-15 17:54:11 +0700

    fix typo

--- app/js/controller.js
+++ app/js/controller.js
@@ -545,7 +545,7 @@ angular.module('reachRabbitApp.controller', ['reachRabbitApp.service'])
         $scope.formData = {};
 
         $scope.adminConfirm = function () {
-            AdminService.confirmPayout($stateParams.transactionId, $scope.formData.slipResource)
+            AdminService.confirmPayout($stateParams.walletId, $scope.formData.slipResource)
                 .then(function () {
                     loadTdoc();
                 })

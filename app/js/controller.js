148dd256e0695c70ad1feecfbf56163ae4dcf974
Laki Sik <laki7877@outlook.com>
2016-09-15 15:50:14 +0700

    fix message

--- app/js/controller.js
+++ app/js/controller.js
@@ -1194,7 +1194,7 @@ angular.module('myApp.brand.controller', ['myApp.service'])
                     .then(function (echoresponse) {
                         $scope.form.$setPristine();
                         if (formData.status === "Open") {
-                            $state.go('brand-campaign-detail-published', { campaignId: echoresponse.data.campaignId, alert: "ลงประกาศเรียบร้อย" });
+                            $state.go('brand-campaign-detail-published', { campaignId: echoresponse.data.campaignId, alert: "แก้ไขข้อมูล และ ลงประกาศเรียบร้อยใหม่เรียบร้อยแล้ว" });
                         } else if (status == "Draft" && echoresponse.data.status == "Draft") {
                             getOne(echoresponse.data.campaignId);
                             $scope.alert.success('บันทึกข้อมูลเรียบร้อยแล้ว!');

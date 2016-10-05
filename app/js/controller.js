d20a770171515e02fc4864dee769715fb1a2dfb9
Natt Phenjati <nattphenjati@gmail.com>
2016-10-05 14:41:41 +0700

    include angular-chart.js and dashboard page

--- app/js/controller.js
+++ app/js/controller.js
@@ -17,6 +17,37 @@ angular.module('reachRabbitApp.controller', ['reachRabbitApp.service'])
             var scope = $scope;
             // console.log("Test World");
         };
+
+        //EXAMPLE CHART.JS PLZ DELETE ME LATER
+        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.series = ['Series A', 'Series B'];
+        $scope.data = [
+         [65, 59, 80, 81, 56, 55, 40],
+         [28, 48, 40, 19, 86, 27, 90]
+        ];
+        $scope.onClick = function (points, evt) {
+         console.log(points, evt);
+        };
+        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+        $scope.options = {
+         scales: {
+           yAxes: [
+             {
+               id: 'y-axis-1',
+               type: 'linear',
+               display: true,
+               position: 'left'
+             },
+             {
+               id: 'y-axis-2',
+               type: 'linear',
+               display: true,
+               position: 'right'
+             }
+           ]
+         }
+        };
+
     }])
     .controller('TransactionDetailController', ['$scope', 'NcAlert', '$stateParams', 'TransactionService', 'AdminService', function ($scope, NcAlert, $stateParams, TransactionService, AdminService) {
         var cartId = $stateParams.cartId;

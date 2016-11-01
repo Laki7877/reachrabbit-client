ff668bc73bdb825a6ded32fa2802bdebfa537e22
Pat Sabpisal <ecegrid@gmail.com>
2016-11-01 17:08:43 +0700

    Update to fit wercker for refactor branch

---
+++ E2E/protractor.conf.auto.js
@@ -0,0 +1,69 @@
+//jshint strict: false
+
+exports.config = {
+
+    allScriptsTimeout: 120000,
+
+    specs: [
+        './e2e-tests/*.js'
+    ],
+
+    capabilities: {
+        'browserName': 'firefox'
+    },
+
+    params: {
+        brand_login: {
+            user: 'brand@reachrabbit.com',
+            password: 'test1234'
+        },
+        influencer_login: {
+            user: 'influencer@reachrabbit.com',
+            password: 'test1234'
+        },
+        god_influencer: {
+            user: 'influencer@reachrabbit.com',
+            password: 'test1234'
+        },
+        facebook_login: {
+            user: 'robot_txcbvac_ng@tfbnw.net',
+            password: 'Ahancer123!'
+        },
+        ig_login: {
+            user: 'eastduckman',
+            password: 'Ahancer123!'
+        },
+        admin_login: {
+            user: 'admin@reachrabbit.com',
+            password: 'test1234'
+        }
+    },
+
+    resultJsonOutputFile: 'test_result/test-report.json',
+    baseUrl: 'https://localhost/',
+    framework: 'jasmine',
+    onPrepare: function() {
+        var SpecReporter = require('jasmine-spec-reporter');
+        // add jasmine spec reporter
+        jasmine.getEnv().addReporter(new SpecReporter({ displayStacktrace: 'none', displaySpecDuration: true }));
+        jasmine.getEnv().addReporter({
+            specDone: function (spec) {
+                if (spec.status === 'failed' || spec.failedExpectations.length > 0) {
+                    // console.dir(spec.failedExpectations.length);
+                    console.log(spec.failedExpectations[0].message);
+                    // console.log(spec.failedExpectations[0].stack);
+                    // browser.pause();
+                }
+            }
+        });
+
+
+    },
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 50000,
+        isVerbose: true,
+        includeStackTrace: true
+    }
+
+};

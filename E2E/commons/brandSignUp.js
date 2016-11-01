0103ba54240736b5bc09d821e3673ab70343f96a
Laki Sik <laki7877@gmail.com>
2016-11-01 11:39:13 +0700

    add more test case

---
+++ E2E/commons/brandSignUp.js
@@ -0,0 +1,51 @@
+var signUpPage = require('../page_objects/brandSignUpPage.js');
+var Chance = require('chance');
+var chance = new Chance();
+
+exports.signUpSuccessCompany = function() {
+    it('Sign up component should exist',function(){
+        expect(signUpPage.form.brandName.isPresent()).toBe(true);
+        expect(signUpPage.form.name.isPresent()).toBe(true);
+        expect(signUpPage.form.phoneNumber.isPresent()).toBe(true);
+        expect(signUpPage.form.email.isPresent()).toBe(true);
+        expect(signUpPage.form.password.isPresent()).toBe(true);
+        expect(signUpPage.form.isCompany.isPresent()).toBe(true);
+        expect(signUpPage.form.submit_btn.isPresent()).toBe(true);
+        signUpPage.form.isCompany.click();
+        expect(signUpPage.form.companyName.isPresent()).toBe(true);
+        expect(signUpPage.form.companyTaxId.isPresent()).toBe(true);
+        expect(signUpPage.form.companyAddress.isPresent()).toBe(true);
+    });
+    it('Should be able to fill sign up form data',function(){
+        signUpPage.form.name.sendKeys(chance.capitalize(chance.word({ length: 10 })));
+        signUpPage.form.brandName.sendKeys(chance.capitalize(chance.word({ length: 10 })) + " Brand");
+        signUpPage.form.phoneNumber.sendKeys(chance.phone({ formatted: false }));
+        signUpPage.form.email.sendKeys(browser.params.brand_login.user);
+        signUpPage.form.password.sendKeys(browser.params.brand_login.password);
+        signUpPage.form.companyName.sendKeys(chance.capitalize(chance.word({ length: 10 })) + " Co Ltd");
+        signUpPage.form.companyTaxId.sendKeys("12341234");
+        signUpPage.form.companyAddress.sendKeys("Somewhere in Space");
+        signUpPage.form.companyAddress.sendKeys(protractor.Key.TAB);
+        signUpPage.form.companyAddress.sendKeys(protractor.Key.ENTER);
+    });
+};
+
+exports.signUpSuccessNoCompany = function() {
+    it('Sign up component should exist',function(){
+        expect(signUpPage.form.brandName.isPresent()).toBe(true);
+        expect(signUpPage.form.name.isPresent()).toBe(true);
+        expect(signUpPage.form.phoneNumber.isPresent()).toBe(true);
+        expect(signUpPage.form.email.isPresent()).toBe(true);
+        expect(signUpPage.form.password.isPresent()).toBe(true);
+        expect(signUpPage.form.isCompany.isPresent()).toBe(true);
+        expect(signUpPage.form.submit_btn.isPresent()).toBe(true);
+    });
+    it('Should be able to fill sign up form data',function(){
+        signUpPage.form.name.sendKeys(chance.capitalize(chance.word({ length: 10 })));
+        signUpPage.form.brandName.sendKeys(chance.capitalize(chance.word({ length: 10 })) + " Brand");
+        signUpPage.form.phoneNumber.sendKeys(chance.phone({ formatted: false }));
+        signUpPage.form.email.sendKeys(browser.params.brand_login.user);
+        signUpPage.form.password.sendKeys(browser.params.brand_login.password);
+    });
+};
+

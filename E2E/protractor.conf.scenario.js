b73f74b56529cebd28a5915d49f93873281c5c22
Laki Sik <laki7877@gmail.com>
2016-11-02 11:16:31 +0700

    protractor for scenrio.js

---
+++ E2E/protractor.conf.scenario.js
@@ -0,0 +1,72 @@
+//jshint strict: false
+exports.config = {
+
+    allScriptsTimeout: 120000,
+
+    specs: [
+        './old-files/scenarios.js'
+    ],
+
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    params: {
+        brand_login: {
+            user: 'brand7887@reachrabbit.com',
+            password: 'test1234'
+        },
+        admin_login: {
+            user: 'undefined',
+            password: 'undefined'
+        },
+        influencer_login: {
+            user: 'influencer@reachrabbit.com',
+            password: 'test1234'
+        },
+        god_influencer: {
+            user: 'influencer@reachrabbit.com',
+            password: 'test1234'
+        },
+        facebook_login: {
+            user: 'robot_txcbvac_ng@tfbnw.net',
+            password: 'Ahancer123!'
+        },
+        ig_login: {
+            user: 'eastduckman',
+            password: 'Ahancer123!'
+        },
+        admin_login: {
+            user: 'admin@reachrabbit.com',
+            password: 'test1234'
+        }
+    },
+
+    resultJsonOutputFile: 'test_result/test-report.json',
+    baseUrl: 'https://localhost/',
+    framework: 'jasmine',
+    onPrepare: function() {
+        var SpecReporter = require('jasmine-spec-reporter');
+        // add jasmine spec reporter
+        jasmine.getEnv().addReporter(new SpecReporter({ displayStacktrace: 'none', displaySpecDuration: true }));
+        jasmine.getEnv().addReporter({
+            specDone: function (spec) {
+                if (spec.status === 'failed' || spec.failedExpectations.length > 0) {
+                    // console.dir(spec.failedExpectations.length);
+                    console.log(spec.failedExpectations[0].message);
+                    // console.log(spec.failedExpectations[0].stack);
+                    // browser.pause();
+                }
+            }
+        });
+
+
+    },
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 50000,
+        isVerbose: true,
+        includeStackTrace: true
+    }
+
+};

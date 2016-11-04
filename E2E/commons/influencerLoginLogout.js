781ec570a25359103137434ec12f27969520b5cd
Laki Sik <laki7877@gmail.com>
2016-11-04 17:19:03 +0700

    clean up

--- E2E/commons/influencerLoginLogout.js
+++ E2E/commons/influencerLoginLogout.js
@@ -14,3 +14,32 @@ exports.gotoLogin = function() {
         browser.executeScript('window.localStorage.clear();');
     });
 };
+
+exports.loginSuccess = function () {
+    it('Sign in component should exist' , function() {
+        expect(loginPage.email.isPresent()).toBe(true);
+        expect(loginPage.password.isPresent()).toBe(true);
+        expect(loginPage.loginBtn.isPresent()).toBe(true);
+        expect(loginPage.signUpBtn.isPresent()).toBe(true);
+    });
+    it('Should be able to fill sign in form data' , function() {
+        var email = browser.params.influencer_login.user;
+        var password = browser.params.influencer_login.password;
+        loginPage.setEmail(email);
+        loginPage.setPassword(password);
+        loginPage.email.getAttribute('value').then(function(value){
+            expect(value.length).toBeGreaterThan(0);
+            expect(value.length).toBeGreaterThan(0);
+        });
+        loginPage.password.getAttribute('value').then(function(value){
+            expect(value.length).toBeGreaterThan(0);
+        });
+        loginPage.clickLogin();
+    });
+    it('Should login successful' , function() {
+        browser.sleep(1000);
+        browser.getCurrentUrl().then(function(actualUrl){
+            expect(actualUrl).toContain('#/influencer-campaign-list');
+        });
+    })
+};
--- E2E/page_objects/influencerLoginPage.js
+++ E2E/page_objects/influencerLoginPage.js
@@ -1,11 +1,19 @@
 var influencerLoginPage = function () {
     'use strict';
 
-    this.email = element(by.model('formData.username'));
-    this.password  = element(by.model('formData.password'));
+    this.email = element(by.model('username'));
+    this.password  = element(by.model('password'));
     this.loginBtn = element(by.id('influencer-login-login-btn'));
     this.signUpBtn = element(by.id('influencer-login-sign-up-btn'));
     //******************** functions *******************//
+    this.setEmail = function (email) {
+        this.email.clear();
+        this.email.sendKeys(email);
+    };
+    this.setPassword = function (password) {
+        this.password.clear();
+        this.password.sendKeys(password);
+    };
     this.clickLogin = function () {
         this.loginBtn.click();
     };

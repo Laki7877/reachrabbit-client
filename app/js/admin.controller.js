e288ed61e8abb09c321a3cb4cb7c715f9c2e9107
Laki Sik <laki7877@gmail.com>
2016-11-28 16:07:39 +0700

    fix admin dashboard

--- app/js/admin.controller.js
+++ app/js/admin.controller.js
@@ -594,4 +594,258 @@ angular.module('reachRabbitApp.admin.controller', ['reachRabbitApp.common.servic
                 chatArea.scrollTop(9999);
             }
         }
-    );
+    )
+    .controller('CampaignDashboardController', function ($scope, $rootScope, $stateParams, CampaignService, DataService, $filter, UserProfile, $uibModal, NcAlert, validator, $state, util) {
+        //initial form data
+        $scope.alert = new NcAlert();
+        $scope.editOpenState = $stateParams.editOpenState;
+        $scope.form = {};
+        util.warnOnExit($scope);
+
+        $scope.resources = [];
+        $scope.formData = {
+            mainResource: null,
+            campaignResources: [],
+            budget: null
+        };
+        $scope.campaignNee = $scope.formData;
+
+        $scope.mediaBooleanDict = {};
+        $scope.mediaObjectDict = {};
+        $scope.categories = [];
+        $scope.budgets = [];
+
+        DataService.getBudgets().then(function (resp) {
+            $scope.budgets = resp.data;
+        });
+
+        $scope.dateOptions = _.extend({}, $rootScope.dateOptions, {
+            minDate: new Date(),
+            customClass: function (object) {
+                if (object.date.getTime() < (new Date()).getTime()) {
+                    return ["nc-dt-button", "nc-dt-disable"];
+                }
+
+                return "nc-dt-button";
+            }
+        });
+        //Setting up form
+        var campaignId = $stateParams.campaignId;
+
+        $scope.budgetDisplayAs = function (budgetObject) {
+            return "สูงสุด " + $filter('number')(budgetObject.toBudget) + " บาท";
+        };
+
+        //Fetch initial datasets
+        DataService.getMedium()
+            .then(function (response) {
+                $scope.medium = response.data;
+                $scope.medium.forEach(function (item) {
+                    $scope.mediaObjectDict[item.mediaId] = item;
+                });
+            })
+            .then(function () {
+                return getOne(campaignId);
+            });
+        DataService.getCategories()
+            .then(function (response) {
+                $scope.categories = response.data;
+            });
+
+        var mediaBooleanDictProcess = function (formData) {
+            formData.media = [];
+            //tell server which media are checked
+            _.forEach($scope.mediaBooleanDict, function (value, key) {
+                if (value === true) {
+                    formData.media.push($scope.mediaObjectDict[key]);
+                }
+            });
+        };
+        $scope.$watch('mediaBooleanDict', function () {
+            mediaBooleanDictProcess($scope.formData);
+        }, true);
+
+        $scope.openCampaign = function () {
+            $state.go('brand-campaign-detail-draft', {
+                campaignId: $scope.campaignNee.campaignId
+            });
+        };
+        // get follower for this influencer
+        function getFollower(data, mediaId) {
+            var counter = 0;
+            _.forEach(data.influencerMedias, function (e) {
+                if (!mediaId || e.media.mediaId === mediaId) {
+                    counter += e.followerCount;
+                }
+            });
+            return counter;
+        }
+        function getDataByMedia(data, mediaId) {
+            var obj = {};
+            obj.influencers = []; // by influencer
+            obj.dataset = [];
+
+            var posts = []; // all posts
+            var keys = null;
+            var postKeys = null;
+            var summator = function (sum, n) {
+                // ignore wrong media
+                if (n.mediaId !== mediaId) {
+                    return sum;
+                }
+                // get key list for sum
+
+                keys = keys ? keys : _.keys(_.pickBy(n, function (value, key) {
+                    return _.startsWith(key, "sum");
+                }));
+                // sum each keys
+                _.forEach(keys, function (k) {
+                    sum[k] = (sum[k] || 0) + n[k];
+                    if (mediaId === 'google' && k === 'sumView') {
+                        // no need to add
+                    } else {
+                        sum.sumEngagement = (sum.sumEngagement || 0) + n[k];
+                    }
+                });
+
+                postKeys = postKeys ? postKeys : _.keys(_.pickBy(sum, function (value, key) {
+                    return _.startsWith(key, "sum");
+                }));
+                // accumulate
+                return sum;
+            };
+            var createDailyFields = function (datasetArray, datasetArrayNext) {
+                if (!datasetArrayNext) {
+                    return function (key) {
+                        var newKey = key.replace('sum', 'daily');
+                        datasetArray[newKey] = (datasetArray[key] || 0);
+                    };
+                }
+                return function (key) {
+                    var newKey = key.replace('sum', 'daily');
+                    datasetArrayNext[newKey] = (datasetArrayNext[key] || 0) - (datasetArray[key] || 0);
+                };
+            };
+
+            // get only post with mediaId
+            _.forEach(data, function (p) {
+                var has = false;
+                var hasPosts = false;
+
+                // get all posts data
+                _.forEach(p.posts, function (e) {
+                    if (e.mediaId === mediaId) {
+                        posts.push(e);
+                        hasPosts = true;
+                    }
+                });
+                _.forEach(p.media, function (e) {
+                    if (e.mediaId === mediaId) {
+                        has = true;
+                    }
+                });
+
+                if (!has && !hasPosts) {
+                    return;
+                }
+                // build influencer data
+                var influencer = {};
+
+                // get per-influencer data
+                influencer.hasPosts = hasPosts;
+                _.extend(influencer, p.influencer);
+                if (influencer.hasPosts) {
+                    var tmp = _.groupBy(p.posts, 'date');
+                    var keys = _.reverse(_.sortBy(_.keys(tmp), function (o) {
+                        return moment(o.date).toDate();
+                    }));
+                    var latestPosts = keys.length > 0 ? tmp[keys[0]] : [];
+                    _.extend(influencer, _.reduce(latestPosts, summator, {}));
+                    influencer.sumFollowerCount = getFollower(p.influencer, mediaId);
+                    influencer.sumEngagementRate = influencer.sumFollowerCount > 0 ? Math.round((influencer.sumEngagement / parseFloat(influencer.sumFollowerCount)) * 10000) / 100.0 : null;
+                }
+                obj.influencers.push(influencer);
+            });
+
+            // build per-media
+
+            // dataset by time
+            var dataset = _.groupBy(posts, 'date');
+            var datasetArray = [];
+            _.forOwn(dataset, function (v, k) {
+                //sum all data up for this timeframe
+                var d = _.reduce(dataset[k], summator, {});
+                d.date = k;
+                datasetArray.push(d);
+            });
+
+            datasetArray = _.sortBy(datasetArray, function (o) {
+                return moment(o.date).toDate();
+            });
+
+            // create daily-fields
+            for (var i = 0; i < datasetArray.length - 1; i++) {
+                if (i === 0) {
+                    _.forEach(postKeys, createDailyFields(datasetArray[i]));
+                }
+                _.forEach(postKeys, createDailyFields(datasetArray[i], datasetArray[i + 1]));
+
+            }
+
+            // fill in per-media data
+            obj.dataset = datasetArray;
+            obj.sumFollowerCount = _.reduce(obj.influencers, function (sum, n) {
+                return sum + n.sumFollowerCount;
+            }, 0);
+            obj.sumInfluencer = obj.influencers.length;
+
+            // totalsum
+            _.extend(obj, datasetArray[datasetArray.length - 1]);
+
+            return obj;
+        }
+        function getData(data) {
+            var obj = {};
+            // global data
+            obj.sumInfluencer = data.length;
+            obj.sumPrice = _.reduce(data, function (sum, n) {
+                return sum + (n.price || 0);
+            }, 0);
+            obj.sumFollowerCount = _.reduce(data, function (sum, n) {
+                return sum + getFollower(n.influencer);
+            }, 0);
+
+            // per-media data
+            obj.media = {};
+            var sumEngagement = 0;
+            _.forOwn($scope.mediaObjectDict, function (v, k) {
+                obj.media[k] = getDataByMedia(data, k);
+                sumEngagement += obj.media[k].sumEngagement || 0;
+            });
+
+            obj.sumCPE = (sumEngagement === 0) ? 0 : Math.round((obj.sumPrice / parseFloat(sumEngagement)) * 1000) / 1000.0;
+
+            return obj;
+        }
+        function getOne(cid) {
+            CampaignService.getOne(cid)
+                .then(function (response) {
+                    //overrides the form data
+                    $scope.formData = angular.copy(response.data);
+                    $scope.mediaBooleanDict = {};
+                    //Tell checkbox which media are in the array
+                    ($scope.formData.media || []).forEach(function (item) {
+                        $scope.mediaBooleanDict[item.mediaId] = true;
+                    });
+
+                    $scope.campaignNee = $scope.formData;
+                    //ensure non null
+                    $scope.formData.keywords = $scope.formData.keywords || [];
+                });
+            CampaignService.getProposals(cid)
+                .then(function (res) {
+                    $scope.dashboard = getData(res.data);
+                    console.log($scope.dashboard, res.data);
+                });
+        }
+    });
